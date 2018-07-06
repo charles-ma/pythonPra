d96df10f0fad87f5cf26c0cd57679b7399c2bccd
Charles Ma <mcrichard.buaa@gmail.com>
2018-07-05 20:40:21 -0700

    adding two sum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pythonPra
 Python playground
+
+Goal: get familiar with Python syntax and able to solve some simple problems using this language.
+Progress will be tracked here in README.md
---
+++ twoSum.py
@@ -0,0 +1,34 @@
+'''
+Given an array of nums, return indices of two elements that sum to target. Guaranteed there is one and only one valid pair in the input array
+'''
+
+import unittest
+
+def twoSum(array, target):
+    if array:
+        hash = {}
+        for i in range(0, len(array)):
+            if (target - array[i]) in hash:
+                return (hash[target - array[i]], i)
+            hash[array[i]] = i                    
+
+# Test cases
+class TwoSumTest(unittest.TestCase):
+    def test_validCase(self):
+        self.assertEqual(twoSum([1, 2, 5, 6], 11), (2, 3))
+        self.assertEqual(twoSum([0, -1, 3], 3), (0, 2))
+        self.assertEqual(twoSum([-1, 6, -3], 3), (1, 2))
+    
+    def test_edgeCase(self):
+        # Special cases for input array
+        self.assertIsNone(twoSum(None, -1))
+        self.assertIsNone(twoSum([], 0))
+        self.assertIsNone(twoSum([1], 5))
+
+        # No valid tuple in array
+        self.assertIsNone(twoSum([-1, -2], 3))
+        self.assertIsNone(twoSum([-2, 2, 2, 6, 9], -10))
+
+# Ran tests only when file/module invoked as main
+if __name__ == '__main__':
+    unittest.main()
